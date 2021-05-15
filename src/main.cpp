666b494353fc5656b016892c47506435f4884fde
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-05-14 23:12:18 -0700

    made CMakeListsUser.txt so IDE code-analysis can see ArduinoBlue.h and updated comments accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,27 @@
 #include <ArduinoBlue.h>
 
 /**
- * CLion PlatformIO vs CMake Important Clarification:
- * SMake is not used for builds, debugging and uploading in a CLion PlatformIO Plugin project, HOWEVER,
- * CMake (or at least some of it's files like CMakeListsPrivate.txt) *IS* used but the CLion IDE/Editor
- * for real-time code analysis and suggestions, particularly related to library location.
- * For example, for the IDE to be able to locate the ArduinoBlue.h file, I had to add this line to the
- * appropriate part of the CMakeListsPrivate.txt file:
- * include_directories("${CMAKE_CURRENT_LIST_DIR}/.pio/libdeps/megaatmega2560/ArduinoBlue/src")
- * Other similar steps may be needed for any 3rd-party library added to a project.
- * TODO: Oops! still learning. Not supposed to change this file. PlatformIO can overwrite this file and it
- * will exist in freshly-created empty projects etc. Proper procedure is to create and add changes/additions
- * into a "CMakeListsUser.txt" file. Will fix this soon. This will be a file we will add to the repo. It is not
- * excluded at the moment so all we need to do is add it, but it might need a conditional around the line we
- * added to be logically correct based on what exists currently in CMakeListsPrivate.txt
+ * CLion PlatformIO vs CMake Important Notes:
+ * These points apply to projects created in CLion using the PlatformIO Plug-In and a standalone (Homebrew)
+ * installation of platformio. CMake is not used for builds but there are make CMake files and directories
+ * created and used by the CLion IDE, but not by PlatformIO. This is important to understand.
+ * Although all build, upload, debug and related actions are performed entirely by PlatformIO, the CLion IDE
+ * itself uses the CMake system simultaneously for providing real-time code-analysis feedback in the Editor
+ * and potentially other features. While you should not need to maintain much in CMake files, for includes
+ * to work correctly you will at least need to create a CMakeListsUser.txt file and add a little code to
+ * it so that the IDE/CMake can locate the includes. For example, for the addition of the ArduinoBlue library
+ * in main.cpp (specifically for #include <ArduinoBlue.h>, we also need the following added to a
+ * new CMakeListsUser.txt file:
+ * if (CMAKE_BUILD_TYPE MATCHES "megaatmega2560")
+ *     include_directories("${CMAKE_CURRENT_LIST_DIR}/.pio/libdeps/megaatmega2560/ArduinoBlue/src")
  */
 
-/**
- * Prior to any HM-10/BLE code, let's just get a Blink program going since this is a brand new IDE and build setup.
- */
 
 void setup() {
 // write your initialization code here
     pinMode(LED_BUILTIN, OUTPUT);
 }
+
 
 void loop() {
 // write your code here
