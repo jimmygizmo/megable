57488ad263eedc7cc006648daf7596eb394e2f8b
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-05-14 21:29:49 -0700

    simple blink example to start with builds, uploads and run successfully. looks like CMake might not be needed at all and PlatformIO-created project works well. docs are outdated. works better and easier than the docs indicate. some past known issues cited in the docs may have already been resolved. All I did was execute the PlatformIO Upload configuration which was already present and it did a build, uploaded and ran on the Mega on the first try.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <Arduino.h>
+
+/**
+ * Prior to any HM-10/BLE code, let's just get a Blink program going since this is a brand new IDE and build setup.
+ */
+
 void setup() {
 // write your initialization code here
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
 // write your code here
+    // Three quick flashes then a long flash
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
