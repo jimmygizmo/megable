2eb68a4a3c375573f0acfd3a579967c5c2569dfc
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-05-16 16:05:46 -0700

    added servo position initialization, adjusted minimum, refactored variable names, added comments for further clarification of variable names.

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,25 @@ Servo myservo;  // create servo object to control a servo
 
 const unsigned long BAUD_RATE = 9600;
 
-const int ANALOG_SERVO_PWM_PIN = 5;
-const int ANALOG_SERVO_MININUM_ANGLE_VALUE = 3; // To stop chatter, we'll limit the lowest angle value to this.
-// The servo minimum angle at which chatter goes away may vary slightly from servo to servo.
-const int ANALOG_SERVO_MAXIMUM_ANGLE_VALUE = 170; // Actual servo travel is almost 195 degrees and we will limit it.
-// In this case we will just stop it where we have measured 180 degrees of travel and this is the value above.
+const int ANALOG_SERVO_AS1_PWM_PIN = 5;  // On the Mega2560, pin 5 is a PWM pin.
+
+const int ANALOG_SERVO_AS1_MINIMUM_ANGLE = 4; // To minimize or stop chatter at the minimum servo position,
+// we'll limit the lowest angle value to the value configured here. This value may vary slightly depending on
+// the individual servo, the exact voltage being used and other factors. Tune/adjust this for each specific servo.
+
+const int ANALOG_SERVO_AS1_MAXIMUM_ANGLE = 170; // Actual servo travel is almost 195 degrees in my case, so we will
+// limit it to right at about 180-182 degrees and having that extra travel will also help with eliminating chatter
+// that always seems to occur with all servos at or near their minimum position.
+// TODO: Address the semantics/meaning of the 'ANGLE' in the variable name now at this point. Since we know
+// most servos can be very inaccurate in terms of true angle values, we might want to just call this 'position'
+// and then mention in a few places that it roughly corresponds to angle, but that we are modifying (limiting
+// and or scaling etc.) values to achieve the desired behavior and simplest/clearest code as well.
+// So the TODO might be to just replace the word 'ANGLE' with 'POSITION' and add some appropriate comments.
+// The terminology 'ANGLE' can be used near the input and more accurately this is 'DESIRED ANGLE' or 'REQUESTED ANGLE'.
+// It is good to pay attention to the semantics of variable naming. Downstream developers will thank you for it and
+// you will avoid confusion and even bugs by have well-named variables, objects, functions, classes etc.
+
+const int ANALOG_SERVO_AS1_INITIAL_ANGLE = ANALOG_SERVO_AS1_MINIMUM_ANGLE;  // initialize at the minimum angle
 
 // The bluetooth tx and rx pins must be supported by software serial.
 // Visit https://www.arduino.cc/en/Reference/SoftwareSerial for unsupported pins.
@@ -37,8 +51,6 @@ ArduinoBlue phone(bluetooth); // pass reference of bluetooth object to ArduinoCo
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
 
-    myservo.attach(ANALOG_SERVO_PWM_PIN);  // attaches the servo on pin 9 to the servo object
-
     // Start serial communications.
     // The baud rate must be the same for both serials.
     Serial.begin(BAUD_RATE);
@@ -47,7 +59,15 @@ void setup() {
     // delay just in case bluetooth module needs time to "get ready".
     delay(200);
 
-    Serial.println("setup complete");
+    Serial.println("Bluetooth initialization complete.");
+
+    myservo.attach(ANALOG_SERVO_AS1_PWM_PIN);  // attaches the servo on the configured pin to the servo object
+
+    Serial.print("\tMoving servo AS1 to initial (minimum chatter-free) position: ");
+    Serial.println(ANALOG_SERVO_AS1_INITIAL_ANGLE);
+    myservo.write(ANALOG_SERVO_AS1_INITIAL_ANGLE);
+
+    Serial.println("Analog servo AS1 initialization complete.");
 }
 
 
@@ -94,10 +114,10 @@ void loop() {
         servo_angle = map(sliderVal, 0, 200, 0, 180);  // scale to use with the servo (value between 0 and 180)
         Serial.print("\tServo angle: ");
         Serial.println(servo_angle);
-        if ( servo_angle < ANALOG_SERVO_MININUM_ANGLE_VALUE ) {
-            adjusted_servo_angle = ANALOG_SERVO_MININUM_ANGLE_VALUE;
-        } else if ( servo_angle > ANALOG_SERVO_MAXIMUM_ANGLE_VALUE ) {
-            adjusted_servo_angle = ANALOG_SERVO_MAXIMUM_ANGLE_VALUE;
+        if ( servo_angle < ANALOG_SERVO_AS1_MINIMUM_ANGLE ) {
+            adjusted_servo_angle = ANALOG_SERVO_AS1_MINIMUM_ANGLE;
+        } else if ( servo_angle > ANALOG_SERVO_AS1_MAXIMUM_ANGLE ) {
+            adjusted_servo_angle = ANALOG_SERVO_AS1_MAXIMUM_ANGLE;
         } else {
             adjusted_servo_angle = servo_angle;
         }
@@ -129,6 +149,12 @@ void loop() {
 //    delay(500);
 //    digitalWrite(LED_BUILTIN, LOW);
 //    delay(500);
+
+// Experimental delay to try to get rid of "chatter during slow movement", which is the big problem at the moment.
+// (Very soon now I will start investigating electromagnetic interference, possibly from the Bluetooth traffic,
+// affecting the servo signal or the servo in some other way. Currently these are very close to each other and the
+// servo wiring is long and going in some big loops nearby so interference is a very real possibility.
+delay(200);
 
 }
 
